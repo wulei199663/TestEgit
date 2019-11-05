f260b2ea512579039262f72dea49db92c80a5a3d
wulei199663 <643922399@qq.com>
2019-11-05 15:45:19 +0800

    更新2

--- src/com/atguigu/egit/test/HelloEgit.java
+++ src/com/atguigu/egit/test/HelloEgit.java
@@ -3,8 +3,8 @@ package com.atguigu.egit.test;
 public class HelloEgit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("HelloEgit.main()");
+System.out.println("更新1");
 	}
 
 }
